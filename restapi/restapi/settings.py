50daa5e0a751b3d8d6ea34d969af8cbae106ba32
dishvater <wwawrzyniak@gmail.com>
2021-06-17 18:12:54 +0200

    change debug to true to avoid status 500 error

--- restapi/restapi/settings.py
+++ restapi/restapi/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 SECRET_KEY = 'z8x5sk#*p%f3dvil=^wmfgw1c2xbcj$rm9wnhhr5yc3$$ocj$#'
 
